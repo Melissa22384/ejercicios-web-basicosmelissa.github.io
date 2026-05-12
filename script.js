d489752c1f03b6febba5858e66d2ca9ef3033ea6
Melissa <alondrayanes77>
2026-05-12 09:40:23 -0600

    arreglos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector("styles.html");
 const header = document.querySelector("mi-encabezado");
+
 const headerHeight = header.offsetHeight;
+
 window.addEventListener("scroll", () => {
     if (window.scrollY > headerHeight) {
         menu.style.backgroundColor = "rgb(50,43,150)";
